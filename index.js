c2e51fd51c4141b824b9563cade974ec823dcc83
Alessandro Pacifici <alessandro.pacifici@gmail.com>
2019-07-13 12:12:39 +0200

    feat(): added webserver, template file, config file, sound, disconnection handler ...

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
 const net = require('net')
-const parseString = require('xml2js').parseString;
+const optionalRequire = require("optional-require")(require)
+const http = require('http')
+const { merge } = require('lodash/fp')
+const fs = require('fs')
+const spawn = require( 'child_process' ).spawn
+
+const TEMPLATE_FILE = 'counterTemplate.html'
+ 
+let template = fs.readFileSync(TEMPLATE_FILE, 'utf8');
+fs.watchFile(TEMPLATE_FILE, () => {
+    template = fs.readFileSync(TEMPLATE_FILE, 'utf8');
+})
+
+
+let actualCounter = "0"
+
+const play = () => spawn( 'cscript.exe', [ './pling.vbs' ] )
+
+play()
+
+const server = http.createServer(function(req, res) {
+  res.writeHead(200)
+  res.end(template.replace('--counter--', actualCounter))
+})
+ 
+const custom = optionalRequire("./config.json") || {}
+const config = merge({
+    remoteIp: "127.0.0.1",
+    remotePort: 5001,
+    localPort: 8080
+}, custom)
+
+const parseString = require('xml2js').parseString
 
 console.log('Starting client')
 var client = new net.Socket();
-client.connect(5001, '127.0.0.1', function() {
-	console.log('Connected');
-	//client.write('Hello, server! Love, Client.');
-});
+
+client.on('connect', () => console.log('Connected'))
+
+const connect = () => {
+    console.log('Connecting to ' + config.remoteIp + ':' +config.remotePort)
+    client.connect(config.remotePort, config.remoteIp)
+}
+
+client.on('error', () => {
+    console.log("Connection error ... trying to reconnect in 1 second")
+    setTimeout(connect, 1000)
+})
+
+client.on('end', (hadError) => {
+    console.log("Connection closed ... trying to reconnect in 1 second")
+    setTimeout(connect, 1000)
+})
 
 client.on('data', function(data) {
     const xml = data.toString('utf8').substring(6)
@@ -17,7 +62,9 @@ client.on('data', function(data) {
                 console.log(err)
             } else {
                 result.operations.call.forEach(element => {
-                    console.dir(element.$.ticketNumber)
+                    console.dir(element)
+                    play()
+                    actualCounter = element.$.ticketNumber
                 })
             }
         })
@@ -25,3 +72,8 @@ client.on('data', function(data) {
         console.log('Empty data')
     }
 })
+
+console.log("Starting http server at port ", config.localPort)
+server.listen(config.localPort)
+
+connect()
